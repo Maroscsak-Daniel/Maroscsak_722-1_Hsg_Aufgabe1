97859bbb5ea0ce93f57bb1e8eaf9f771c494b780
Maroscsak Daniel <daniel.maroscsak@gmail.com>
2025-10-20 15:04:55 +0300

    Finally done with ts

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,6 @@ public class Main {
     public static void main(String[] args) {
         int[] noten = {84, 29, 57, 38, 73, 99};
         NotenSystem allenoten = new NotenSystem(noten);
-
         System.out.println("Nicht ausreichende Noten: " + Arrays.toString(allenoten.nichtAusreichendeNoten()));
         System.out.println("Durchschnitt: " + allenoten.durchschnitt());
         System.out.println("Abgerundete Noten: " + Arrays.toString(allenoten.abgerundeteNoten()));
